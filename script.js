cccbeefaaeb0325113be777b170ce961ea46545a
Olga Braiko <olga.braiko456@mail.ru>
2023-03-29 05:38:47 -0700

    Geners

--- script.js
+++ script.js
@@ -1,124 +1,89 @@
-/*let numberOfFilms;
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-  while (
-    numberOfFilms === "" ||
-    numberOfFilms === null ||
-    isNaN(numberOfFilms)
-  ) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-  }
-}
-start();
-
 const personalMovieDB = {
-  count: "Сколько фильмов вы уже посмотрели?",
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
+  start: () => {
+    personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (
+      personalMovieDB.count === "" ||
+      personalMovieDB.count === null ||
+      isNaN(personalMovieDB.count)
+    ) {
+      personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+  },
+
+  rememberMyFilm: () => {
+    for (let i; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("Hа сколько оцените его?", "");
+
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+      } else {
+        console.log("Error");
+        i - 1;
+      }
+    }
+  },
+
+  detectPerconalLevel: () => {
+    if (personalMovieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+
+  showMyDB: (hidden) => {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+
+  toggleVisibleMyDB: () => {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  },
+
+  writeYouGenres: () => {
+    for (let i = 1; i <= 3; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`, "");
+      if (genre === "" || genre == null) {
+        console.log("Вы ввели некорректные данные");
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
+      }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1}- это ${item}`);
+    });
+  },
 };
-
-const c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("Hа сколько оцените его?", "");
-
-personalMovieDB.movies[c] = d;
-
-function detectPerconalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
-detectPerconalLevel();
 
 console.log(personalMovieDB);
 
-function rememberMyFilms() {
-  for (let i; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("Hа сколько оцените его?", "");
-
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log("done");
-    } else {
-      console.log("Error");
-      i - 1;
-    }
-  }
+if (browser === "Edge") {
+  console.log("You've got the Edge!");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  console.log("Okay we support these browsers too");
+} else {
+  console.log("We hope that this page looks ok!");
 }
-rememberMyFilms();
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYouGenres() {
-  for (let i = 1; i <= 3; i++) {
-    const genere = prompt(`Ваш любимый жанр под номером ${i}`, "");
-    personalMovieDB.genres[i - 1] = genere;
-  }
-}
-writeYouGenres();*/
-
-/*function first() {
-  setTimeout(function () {
-    console.log(1);
-  }, 500);
-}
-
-function second() {
-  console.log(2);
-}
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`I lerning ${lang}`);
-  callback();
-}
-learnJS("JS", function () {
-  console.log("Hello world");
-});*/
-
-/*const users = [
-    { name: "John Doe", accessLevel: "admin" },
-    { name: "Jane Smith", accessLevel: "user" },
-    { name: "Bob Johnson", accessLevel: "user" },
-    { name: "Alice Brown", accessLevel: "admin" },
-  ];
-  
-  const admins = users.filter((user) => user.accessLevel === "admin");
-  const person = users.filter((user) => user.accessLevel === "user");
-  
-  console.log({ admins, person });
-  
-  const lengthAdmin = admins;
-  console.log(lengthAdmin.length);
-  const lengthUser = person;
-  console.log(lengthUser.length);*/
-
-const btnPlus = document.querySelector(".counter__btn-plus");
-const btnMinus = document.querySelector(".counter__btn-minus");
-const counter = document.querySelector(".counter");
-const countValue = document.querySelector(".counter__value");
-
-let count = 0;
-
-btnPlus.addEventListener("click", (e) => {
-  countValue.textContent = --count;
-});
-
-btnMinus.addEventListener("click", (e) => {
-  countValue.textContent = ++count;
-});
