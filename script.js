97a138280c84891c8dfb11022ac61f0cbbae4b64
Olga Braiko <olga.braiko456@mail.ru>
2023-03-30 08:58:35 -0700

    car

--- script.js
+++ script.js
@@ -1,124 +1,96 @@
-/*let numberOfFilms;
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const catalog = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    rating: { rate: 4.1, count: 259 },
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price: 55.99,
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    rating: { rate: 4.7, count: 500 },
+  },
+  {
+    id: 4,
+    title: "Mens Casual Slim Fit",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    rating: { rate: 2.1, count: 430 },
+  },
+  {
+    id: 5,
+    title:
+      "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price: 695,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 4.6, count: 400 },
+  },
+];
 
-  while (
-    numberOfFilms === "" ||
-    numberOfFilms === null ||
-    isNaN(numberOfFilms)
-  ) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+class Products {
+  render() {
+    let htmlCatalog = "";
+    catalog.forEach(
+      ({ id, title, price, image, description, category, rating }) => {
+        htmlCatalog += `
+            <li>
+            <title>${title}</title>
+            <img src ='${image}' />
+            <span>${price}</span>
+            <span>${description}</span>
+            <span>${category}</span>
+            <span>${rating}</span>
+             
+            </li>
+            
+            `;
+      }
+    );
+    const html = `
+        <ul>
+        ${htmlCatalog}
+        </ul>
+        `;
+
+    cardFirst.innerHTML = html;
+    cardSecond.innerHTML = html;
+    cardThird.innerHTML = html;
+    cardFourth.innerHTML = html;
+    cardFifth.innerHTML = html;
   }
 }
-start();
 
-const personalMovieDB = {
-  count: "Сколько фильмов вы уже посмотрели?",
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+const productsPage = new Products();
+productsPage.render();
 
-const c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("Hа сколько оцените его?", "");
-
-personalMovieDB.movies[c] = d;
-
-function detectPerconalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
-detectPerconalLevel();
-
-console.log(personalMovieDB);
-
-function rememberMyFilms() {
-  for (let i; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("Hа сколько оцените его?", "");
-
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log("done");
-    } else {
-      console.log("Error");
-      i - 1;
-    }
-  }
-}
-rememberMyFilms();
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYouGenres() {
-  for (let i = 1; i <= 3; i++) {
-    const genere = prompt(`Ваш любимый жанр под номером ${i}`, "");
-    personalMovieDB.genres[i - 1] = genere;
-  }
-}
-writeYouGenres();*/
-
-/*function first() {
-  setTimeout(function () {
-    console.log(1);
-  }, 500);
-}
-
-function second() {
-  console.log(2);
-}
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`I lerning ${lang}`);
-  callback();
-}
-learnJS("JS", function () {
-  console.log("Hello world");
-});*/
-
-/*const users = [
-    { name: "John Doe", accessLevel: "admin" },
-    { name: "Jane Smith", accessLevel: "user" },
-    { name: "Bob Johnson", accessLevel: "user" },
-    { name: "Alice Brown", accessLevel: "admin" },
-  ];
-  
-  const admins = users.filter((user) => user.accessLevel === "admin");
-  const person = users.filter((user) => user.accessLevel === "user");
-  
-  console.log({ admins, person });
-  
-  const lengthAdmin = admins;
-  console.log(lengthAdmin.length);
-  const lengthUser = person;
-  console.log(lengthUser.length);*/
-
-const btnPlus = document.querySelector(".counter__btn-plus");
-const btnMinus = document.querySelector(".counter__btn-minus");
-const counter = document.querySelector(".counter");
-const countValue = document.querySelector(".counter__value");
-
-let count = 0;
-
-btnPlus.addEventListener("click", (e) => {
-  countValue.textContent = --count;
-});
-
-btnMinus.addEventListener("click", (e) => {
-  countValue.textContent = ++count;
-});
+const cardFirst = document.getElementById("1");
+const cardSecond = document.getElementById("2");
+const cardThird = document.getElementById("3");
+const cardFourth = document.getElementById("4");
+const cardFifth = document.getElementById("5");
